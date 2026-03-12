56eeb49250cd557788786550c5fb0a4ecc405b3c
Kara Diaby <kdiaby.pro@gmail.com>
2026-03-12 15:35:25 +0100

    feat(admin): integrate selector component into form editor

--- app/components/types_de_champ_editor/champ_component.rb
+++ app/components/types_de_champ_editor/champ_component.rb
@@ -69,44 +69,6 @@ class TypesDeChampEditor::ChampComponent < ApplicationComponent
     @focused ? { controller: 'autofocus' } : nil
   end
 
-  def types_of_type_de_champ
-    cat_scope = "activerecord.attributes.type_de_champ.categorie"
-    tdc_scope = "activerecord.attributes.type_de_champ.type_champs"
-    TypeDeChamp.type_champs.keys
-      .filter(&method(:filter_type_champ))
-      .filter(&method(:filter_featured_type_champ))
-      .filter(&method(:filter_block_type_champ))
-      .filter(&method(:filter_public_or_private_only_type_champ))
-      .group_by { TypeDeChamp::TYPE_DE_CHAMP_TO_CATEGORIE.fetch(_1.to_sym) }
-      .sort_by { |k, _v| TypeDeChamp::CATEGORIES.find_index(k) }
-      .to_h do |cat, tdc|
-        [
-          t(cat, scope: cat_scope),
-          tdc.map { [t(_1, scope: tdc_scope), _1, { disabled: !accepted_type_champs.include?(_1) }] },
-        ]
-      end
-  end
-
-  ACCEPTED_TYPES = Columns::ChampColumn::CAST.keys
-    .group_by { |(from)| from.to_s }
-    .transform_values { |pairs| pairs.map { |(_, to)| to.to_s } }
-
-  def accepted_type_champs
-    @accepted_type_champs ||= if published_type_champ.present?
-      ([published_type_champ] + ACCEPTED_TYPES.fetch(published_type_champ, [])).uniq
-    else
-      TypeDeChamp.type_champs.keys
-    end
-  end
-
-  def published_type_champ
-    @published_type_champ ||= procedure.published_revision&.types_de_champ&.find { _1.stable_id == type_de_champ.stable_id }&.type_champ
-  end
-
-  def disabled_type_de_champ_select?
-    coordinate.used_by_routing_rules? || coordinate.used_by_ineligibilite_rules? || accepted_type_champs.size == 1
-  end
-
   def piece_justificative_template_options
     {
       attached_file: type_de_champ.piece_justificative_template,
@@ -123,49 +85,6 @@ class TypesDeChampEditor::ChampComponent < ApplicationComponent
     }
   end
 
-  EXCLUDE_FROM_BLOCK = [
-    TypeDeChamp.type_champs.fetch(:repetition),
-    TypeDeChamp.type_champs.fetch(:quotient_familial),
-  ]
-
-  def filter_block_type_champ(type_champ)
-    !coordinate.child? || !EXCLUDE_FROM_BLOCK.include?(type_champ)
-  end
-
-  def filter_public_or_private_only_type_champ(type_champ)
-    if coordinate.private?
-      !TypeDeChamp::PUBLIC_ONLY_TYPES.include?(type_champ)
-    else
-      !TypeDeChamp::PRIVATE_ONLY_TYPES.include?(type_champ)
-    end
-  end
-
-  def filter_featured_type_champ(type_champ)
-    feature_name = TypeDeChamp::FEATURE_FLAGS[type_champ.to_sym]
-    feature_name.blank? || procedure.feature_enabled?(feature_name)
-  end
-
-  def filter_type_champ(type_champ)
-    if type_champ == TypeDeChamp.type_champs.fetch(:titre_identite)
-      return type_de_champ.titre_identite?
-    end
-
-    case type_champ
-    when TypeDeChamp.type_champs.fetch(:number)
-      has_legacy_number?
-    when TypeDeChamp.type_champs.fetch(:cnaf)
-      procedure.cnaf_enabled?
-    when TypeDeChamp.type_champs.fetch(:dgfip)
-      procedure.dgfip_enabled?
-    when TypeDeChamp.type_champs.fetch(:pole_emploi)
-      procedure.pole_emploi_enabled?
-    when TypeDeChamp.type_champs.fetch(:mesri)
-      procedure.mesri_enabled?
-    else
-      true
-    end
-  end
-
   def format_families_for_select
     return [] if !defined?(FORMAT_FAMILIES)
 
@@ -176,10 +95,6 @@ class TypesDeChampEditor::ChampComponent < ApplicationComponent
         (defined?(FORMAT_FAMILY_EXAMPLES) && FORMAT_FAMILY_EXAMPLES[key]),
       ]
     end
-  end
-
-  def has_legacy_number?
-    revision.types_de_champ.any?(&:number?)
   end
 
   def options_for_character_limit
