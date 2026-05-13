fc65838b48d1caab4b3d9a3ec0258c444d6f83d2
simon lehericey <mail@simon.lehericey.net>
2026-05-13 10:22:19 +0200

    fix(drop_down_column): one can filter on a other_option on drop_down

--- app/models/type_de_champ.rb
+++ app/models/type_de_champ.rb
@@ -503,7 +503,7 @@ class TypeDeChamp < ApplicationRecord
       if drop_down_advanced?
         Array.wrap(referentiel&.options_for_select)
       else
-        drop_down_options.uniq.map { [_1, _1] }
+        options_for_select_with_other
       end
     elsif yes_no?
       Champs::YesNoChamp.options
@@ -515,11 +515,17 @@ class TypeDeChamp < ApplicationRecord
   end
 
   def options_for_select_with_other
-    if drop_down_other?
-      options_for_select + [[I18n.t('shared.champs.drop_down_list.other'), Champs::DropDownListChamp::OTHER]]
+    options = if drop_down_advanced?
+      Array.wrap(referentiel&.options_for_select)
     else
-      options_for_select
+      drop_down_options.uniq.map { [it, it] }
     end
+
+    if drop_down_other?
+      options << [I18n.t('shared.champs.drop_down_list.other'), Champs::DropDownListChamp::OTHER]
+    end
+
+    options
   end
 
   def drop_down_options_from_text=(text)
