5e35db6e11dcb0d5c992c786f7c69c7fc28a41d2
Kara Diaby <kdiaby.pro@gmail.com>
2026-05-13 12:01:02 +0200

    feat(personnalisation): persist user personnalisation

--- app/controllers/users/dossiers_personnalisation_controller.rb
+++ app/controllers/users/dossiers_personnalisation_controller.rb
@@ -8,11 +8,53 @@ module Users
       @procedures_presentations = load_procedures_presentations
     end
 
+    def update
+      permitted_presentations.each do |procedure_id, attrs|
+        procedure = user_procedure_from_param(procedure_id)
+        next if procedure.blank?
+
+        persist_presentation(procedure, Array(attrs[:displayed_column_ids]))
+      end
+
+      redirect_to dossiers_path, notice: t('users.dossiers_personnalisation.flash.success')
+    end
+
     private
 
     def ensure_eligible
       return if current_user.dossiers_personnalisables?
       redirect_to dossiers_path
+    end
+
+    def permitted_presentations
+      params.fetch(:presentations, {}).permit!.to_h
+    end
+
+    def user_procedure_from_param(procedure_id)
+      Procedure
+        .joins(:dossiers)
+        .where(dossiers: { user_id: current_user.id })
+        .where(id: procedure_id)
+        .first
+    end
+
+    def persist_presentation(procedure, stable_ids)
+      columns = valid_columns_for(procedure, stable_ids)
+      presentation = current_user.user_procedure_presentations.find_or_initialize_by(procedure_id: procedure.id)
+
+      if columns.empty?
+        presentation.destroy if presentation.persisted?
+      else
+        presentation.update!(displayed_columns: columns)
+      end
+    end
+
+    def valid_columns_for(procedure, stable_ids)
+      authorized_types_de_champ = procedure.types_de_champ_personnalisables.where(stable_id: stable_ids).index_by { |tdc| tdc.stable_id.to_s }
+      stable_ids.filter_map do |sid|
+        type_de_champ = authorized_types_de_champ[sid.to_s]
+        type_de_champ&.columns(procedure:)&.first
+      end
     end
 
     def load_procedures_presentations
--- spec/requests/users/dossiers_personnalisation_spec.rb
+++ spec/requests/users/dossiers_personnalisation_spec.rb
@@ -7,6 +7,42 @@ RSpec.describe 'Users::DossiersPersonnalisation', type: :request do
 
   before { login_as(user, scope: :user) }
 
+  describe 'PATCH /dossiers/personnalisation' do
+    let(:procedure) { create(:procedure, :published, types_de_champ_public: [{ type: :text, libelle: 'Titre' }]) }
+    let!(:dossiers) { create_list(:dossier, 6, user:, procedure:) }
+    let(:type_de_champ) { procedure.active_revision.types_de_champ.first }
+
+    it 'crée la presentation et redirige avec flash' do
+      patch users_dossiers_personnalisation_path, params: {
+        presentations: { procedure.id.to_s => { displayed_column_ids: [type_de_champ.stable_id.to_s] } },
+      }
+
+      expect(response).to redirect_to(dossiers_path)
+      expect(flash[:notice]).to eq(I18n.t('users.dossiers_personnalisation.flash.success'))
+      expect(UserProcedurePresentation.find_by(user:, procedure:).displayed_columns.size).to eq(1)
+    end
+
+    it 'détruit la presentation existante si la sélection est vide' do
+      UserProcedurePresentation.create!(user:, procedure:, displayed_columns: type_de_champ.columns(procedure:))
+
+      patch users_dossiers_personnalisation_path, params: {
+        presentations: { procedure.id.to_s => { displayed_column_ids: [] } },
+      }
+
+      expect(UserProcedurePresentation.find_by(user:, procedure:)).to be_nil
+    end
+
+    it 'ignore les procedure_ids étrangères' do
+      autre_procedure = create(:procedure, :published)
+
+      patch users_dossiers_personnalisation_path, params: {
+        presentations: { autre_procedure.id.to_s => { displayed_column_ids: ['1'] } },
+      }
+
+      expect(UserProcedurePresentation.find_by(user:, procedure: autre_procedure)).to be_nil
+    end
+  end
+
   describe 'GET /dossiers/personnalisation/edit' do
     context 'avec 5 dossiers ou moins' do
       before { create_list(:dossier, 5, user:) }
