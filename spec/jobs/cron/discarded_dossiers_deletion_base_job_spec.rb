a1fd71b1ddfaa643ca146d95d2b84bf5825e3a92
Colin Darie <colin@darie.eu>
2026-05-20 22:54:19 +0200

    test(jobs): verify BATCH_LIMIT triggers re-enqueue

--- spec/jobs/cron/discarded_dossiers_deletion_base_job_spec.rb
+++ spec/jobs/cron/discarded_dossiers_deletion_base_job_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe Cron::DiscardedDossiersDeletionBaseJob, type: :job do
       expect(described_class::BATCH_LIMIT).to eq(100)
     end
   end
+
+  describe '#perform with BATCH_LIMIT drainage' do
+    let(:dossier_ids) { create_list(:dossier, 3).map(&:id) }
+    let(:job_class) do
+      ids = dossier_ids
+      Class.new(described_class).tap do |klass|
+        klass.define_singleton_method(:name) { 'TestDrainJob' }
+        klass.define_method(:scope) { Dossier.where(id: ids) }
+      end
+    end
+
+    before do
+      stub_const("#{described_class}::BATCH_LIMIT", 2)
+    end
+
+    it 'processes exactly BATCH_LIMIT dossiers and re-enqueues self when more remain' do
+      expect {
+        job_class.perform_now
+      }.to change { Dossier.where(id: dossier_ids).count }.from(3).to(1)
+        .and have_enqueued_job(job_class)
+    end
+  end
 end
