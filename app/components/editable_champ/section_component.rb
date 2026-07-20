88f9568e51ea7f6e4c16ad3c864bc76d94d233d9
Paul Chavard <pro@paul.chavard.net>
2026-07-20 16:39:57 +0200

    tech: refactor EditableChamp::SectionComponent with the tree api

--- app/components/editable_champ/section_component.rb
+++ app/components/editable_champ/section_component.rb
@@ -4,54 +4,32 @@ class EditableChamp::SectionComponent < ApplicationComponent
   include ApplicationHelper
   include TreeableConcern
 
-  def initialize(dossier:, nodes: nil, types_de_champ: nil, row_id: nil, row_number: nil)
-    nodes ||= to_tree(types_de_champ:)
-    @dossier = dossier
-    @row_id = row_id
-    @row_number = row_number
-    @nodes = to_fieldset(nodes:)
+  def initialize(dossier:, types_de_champ: nil, header_section: nil, row_id: nil, row_number: nil)
+    @dossier, @header_section, @row_id, @row_number = dossier, header_section, row_id, row_number
+    @nodes = types_de_champ ? to_tree_roots(types_de_champ:) : header_section.children(dossier.revision)
   end
 
   def render_within_fieldset?
-    first_champ_is_an_header_section?
+    @header_section.present?
   end
 
   def header_section
-    node = @nodes.first
-    @dossier.project_champ(node, row_id: @row_id) if node.is_a?(TypeDeChamp) && node.header_section?
+    @dossier.project_champ(@header_section, row_id: @row_id) if @header_section
   end
 
   def splitted_tail
-    tail.map { split_section_champ(_1) }
-  end
-
-  def tail
-    return @nodes if !first_champ_is_an_header_section?
-    _, *rest_of_champ = @nodes
-
-    rest_of_champ
+    @nodes.map { split_section_champ(it) }
   end
 
   def tag_for_depth
     "h#{header_section.level + 1}"
   end
 
-  def split_section_champ(node)
-    case node
-    when EditableChamp::SectionComponent
-      [node, nil]
+  def split_section_champ(type_de_champ)
+    if type_de_champ.header_section?
+      [EditableChamp::SectionComponent.new(dossier: @dossier, header_section: type_de_champ, row_id: @row_id), nil]
     else
-      [nil, @dossier.project_champ(node, row_id: @row_id)]
+      [nil, @dossier.project_champ(type_de_champ, row_id: @row_id)]
     end
-  end
-
-  private
-
-  def to_fieldset(nodes:)
-    nodes.map { _1.is_a?(Array) ? EditableChamp::SectionComponent.new(dossier: @dossier, nodes: _1, row_id: @row_id) : _1 }
-  end
-
-  def first_champ_is_an_header_section?
-    header_section.present?
   end
 end
