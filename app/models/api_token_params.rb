202f27926eb7bd3d9ee724ffb76e29c7512eab0d
Lisa Durand <lisa.c.durand@gmail.com>
2026-09-09 16:11:35 +0200

    feat(api_tokens): allow admin to duplicate an existing api token

--- app/models/api_token_params.rb
+++ app/models/api_token_params.rb
@@ -13,11 +13,27 @@ class APITokenParams
     params[:name]
   end
 
+  def duplicating?
+    params[:api_token_to_duplicate].present?
+  end
+
   def to_h
     FIELDS.index_with { |field| params[field] }.compact
   end
 
   def hidden_fields_for(owned_fields)
     to_h.except(*owned_fields)
+  end
+
+  def self.from_token(token)
+    new(ActionController::Parameters.new(
+      api_token_to_duplicate: token.id,
+      name: token.name,
+      target: token.allowed_procedure_ids.blank? ? 'all' : 'custom',
+      targets: token.allowed_procedure_ids,
+      access: token.write_access? ? 'read_write' : 'read',
+      networkFiltering: token.authorized_networks.blank? ? 'autoAssign' : 'customNetworks',
+      networks: token.authorized_networks.presence&.map { |ip| "#{ip}/#{ip.prefix}" }&.join(' ')
+    ))
   end
 end
